175590252ee1254bae98f206a2142136f3d6ee9f
Dusly Nestor <2395223@collegemv.qc.ca>
2025-12-02 12:18:28 -0500

    actionFileReader complet

--- src/main/java/mv/sdd/io/ActionFileReader.java
+++ src/main/java/mv/sdd/io/ActionFileReader.java
@@ -1,5 +1,7 @@
 package mv.sdd.io;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +13,34 @@ public class ActionFileReader {
 
         // TODO : Ajouter le code qui permet de lire et parser un fichier d'actions
 
+        //ouverture fichier en lecture
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            //lecture par ligne
+            while ((line = br.readLine()) != null) {
+                //enlever espaces de début fin
+                line = line.trim();
+
+                //ignorer lignes vides ou commentaires('#')
+                if (line.isEmpty() || line.startsWith("#")) {
+                    continue;
+                }
+
+                //parser transforme ligne -> Action
+                Action action = ActionParser.parseLigne(line);
+
+                // Ajouter dans liste actions
+                actions.add(action);
+            }
+        } catch (IOException e) {
+            //erreurs de lecture
+            throw new IOException("Erreur lors de la lecture du fichier d'actions : " + e.getMessage(), e);
+        }
+
+        //liste actions lues
         return actions;
+
+
     }
 }
